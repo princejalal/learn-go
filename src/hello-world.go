7183af9ad25e8980ccaaec8199f6e17ccfce81b1
jalal alirezaei <jalalpoker1@gamil.com>
2022-09-18 21:46:29 +0430

    first commit for start learning golang

---
+++ src/hello-world.go
@@ -0,0 +1,10 @@
+package main
+
+import ("fmt")
+
+func main() {
+//     var name = "jalal";
+//     fmt.Println(name);
+var Y int16 = 32767
+    fmt.Println(Y+2, Y-2)
+}
---
+++ src/variable.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var height, width = 55, 100
+	// count := 10 // short hand for declare variable
+	name, count := "jalal", 20 // declare multiple variables
+	fmt.Println(count, name)
+}
